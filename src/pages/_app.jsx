4afc65446b960888eed6ba304687c86b75be47ed
Kim Björkman <bjorkman.art@gmail.com>
2022-04-07 20:03:40 +0200

    add fathom

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,7 @@
 
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import * as Fathom from 'fathom-client';
 import ErrorBoundary from '@/components/dom/error-boundary';
 import { darkTheme, lightTheme } from '@/theme/stitches.config';
 import { globalStyles } from "@/theme/globalStyles"
@@ -9,6 +11,30 @@ import Box from '@/components/dom/box';
 function MyApp({ Component, pageProps }) {
   const [dark, setDark] = useState(false)
   const prefersDarkMode = usePrefersDarkMode();
+  const router = useRouter();
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    // Example: yourdomain.com
+    //  - Do not include https://
+    //  - This must be an exact match of your domain.
+    //  - If you're using www. for your domain, make sure you include that here.
+    Fathom.load('YANNQPPK', {
+      includedDomains: ['cv.bjorkman.kim', 'wwww.cv.bjorkman.kim', 'bjorkman.kim', 'www.bjorkman.kim'],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   useEffect(() => {
     setDark(prefersDarkMode)
